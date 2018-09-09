71f6e02282f5f0561bd687bf68b5e264175c63f8
yanka yohana <yanka.neves@ccc.ufcg.edu.br>
2018-09-09 20:04:09 -0300

    first commit

--- src/lab2/ContaLaboratorio.java
+++ src/lab2/ContaLaboratorio.java
@@ -1,29 +1,70 @@
 package lab2;
 
+/**
+ * Essa Classe contém informações sobre as contas dos alunos nos laboratórios,
+ * tais como: nome do laboratório, cota, e espaço utilizado.
+ * 
+ * @author Yanka Yohana, 116211079 - Laboratório de Programaçao 2
+ */
+
 public class ContaLaboratorio {
 
+	/**
+	 * String que representa o nome do laboratório.
+	 */
 	private String nome;
+
+	/**
+	 * Inteiro que representa o limite de espaço.
+	 */
 	private int cota;
+
+	/**
+	 * Inteiro que representa a quantidade de espaço utilizado.
+	 */
 	private int espacoUtilizado;
 
+	/**
+	 * Constrói uma conta no laboratório a partir do nome do laboratório.
+	 * 
+	 * @param nome
+	 */
 	public ContaLaboratorio(String nome) {
-		this.nome = nome;
-		this.cota = 2000;
+		this(nome, 2000);
 	}
 
+	/**
+	 * Constrói uma conta no laboratório a partir do nome do laboratório e de uma
+	 * cota.
+	 */
 	public ContaLaboratorio(String nomeLaboratorio, int cota) {
 		this.nome = nomeLaboratorio;
 		this.cota = cota;
 	}
 
+	/**
+	 * Adiciona espaço utilizado na conta.
+	 * 
+	 * @param mbytes cota medida em mb
+	 */
 	public void consomeEspaco(int mbytes) {
 		this.espacoUtilizado += mbytes;
 	}
 
+	/**
+	 * Libera espaço na conta.
+	 * 
+	 * @param mbytes cota medida em mb
+	 */
 	public void liberaEspaco(int mbytes) {
 		this.espacoUtilizado -= mbytes;
 	}
 
+	/**
+	 * verifica se o aluno atingiu a cota de espaço.
+	 * 
+	 * @return
+	 */
 	public boolean atingiuCota() {
 		boolean atingiu = false;
 		if (espacoUtilizado >= cota) {
